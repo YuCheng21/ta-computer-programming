67a2d1d3b362d013e484c7756469b8e016517040
YuCheng <fyc621@gmail.com>
2022-03-18 19:03:59 +0800

    adjust: config #20220318001

--- config.py
+++ config.py
@@ -1,17 +1,38 @@
-week = {
-    'week1': {
-        'Title': '一元二次方程式',
-        'Notes': '(2a) 要加括號',
-        'file_input': ["4", "32", "28"],
-        # 'FILE_INPUT': ["1", "6", "8"],
-        'output_pattern': [[r'(-1)+', r'(-7)+']],
-        # 'FILE_INPUT': ["1", "6", "8"],
-    },
-    'week2': {
-        'Notes': '課堂完成',
-    },
-    'week3': {
-        'file_input': ["2", "-2"],
-        'output_pattern': [[r'(0\.25)+'], [r'(1/4)+']],
-    },
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('env.ini')
+app = config['app']
+
+# App
+target_week = app.get('TargetWeek', 'week1')
+debug = app.getboolean('Debug', False)
+
+# Grading
+correct = 100
+wrong = 90
+compile_fail = 80
+not_paid = 0
+
+# Week Data
+week_args = dict()
+week_args['week1'] = {
+    'Title': '一元二次方程式公式解',
+    'Notes': '(2a) 要加括號',
+    'file_input': ["4", "32", "28"],
+    # 'FILE_INPUT': ["1", "6", "8"],
+    'output_pattern': [[r'(-1)+', r'(-7)+']],
+    # 'FILE_INPUT': ["1", "6", "8"],
 }
+week_args['week2'] = {
+    'Notes': '課堂完成（一元二次重根與無解）',
+}
+week_args['week3'] = {
+    'file_input': ["2", "-2"],
+        'output_pattern': [[r'(0\.25)+'], [r'(1/4)+']],
+}
+week_args['week4'] = {
+    'Notes': '課堂完成（算圓周率）',
+}
+
--- main.py
+++ main.py
@@ -5,35 +5,55 @@ import codecs
 from pathlib import Path
 import pandas as pd
 from datetime import datetime
-from config import week
+import logging, sys
+import config
 
-target_week = 'week1'
 
+# Config
+TARGET_WEEK = config.target_week
+DEBUG = config.debug
 # Grading
-CORRECT = 100
-WRONG = 90
-COMPILE_FAIL = 80
-NOT_PAID = 0
+CORRECT = config.correct
+WRONG = config.wrong
+COMPILE_FAIL = config.compile_fail
+NOT_PAID = config.not_paid
 # Compiler
 COMPILE_OUTPUT = 'compile_result'
 COMPILER_COMMAND = 'gcc'
 COMPILER_ARGUMENT = '-lstdc++ -lm -w'
 COMPILER_ARGUMENT_BIG5 = '-finput-charset=big5'
 # Executor
-TRAVERSE_DIRECTORY = f"./{target_week}"
-FILE_INPUT = week[target_week]['file_input']
-OUTPUT_PATTERN = week[target_week]['output_pattern']
+TRAVERSE_DIRECTORY = f"./{TARGET_WEEK}"
+FILE_INPUT = config.week_args[TARGET_WEEK]['file_input']
+OUTPUT_PATTERN = config.week_args[TARGET_WEEK]['output_pattern']
 CSV_FOLDER = "./result"
 CSV_FILENAME = f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.csv"
 
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stderr,
+    level=logging.DEBUG if DEBUG else logging.INFO,
+)
 
-def encoding_to_utf8(file_path):
-    try:
-        file = codecs.open(file_path, 'r', 'gbk').read()
-    except UnicodeDecodeError as error:
-        pass
-    else:
-        codecs.open(file_path, 'w', 'utf-8').write(file)
+# def encoding_to_utf8(file_path):
+#     try:
+#         file = codecs.open(file_path, 'r', 'gbk').read()
+#     except UnicodeDecodeError as error:
+#         pass
+#     else:
+#         codecs.open(file_path, 'w', 'utf-8').write(file)
+
+
+def check_file_ext(root, file):
+    input_file = f'{root}/{file}'
+    pattern = r'\.(c|cpp)$'
+    regex = re.search(pattern, input_file)
+    if regex is None:
+        logging.debug(f'file ext not allow')
+        return False
+    logging.debug(f'file ext correct')
+    return True
 
 
 def compiler(root, file):
@@ -42,17 +62,21 @@ def compiler(root, file):
 
     # encoding_to_utf8(input_file)
 
-    pattern = r'\.(c|cpp)$'
-    regex = re.search(pattern, input_file)
-    if regex is None:
-        return None
-    else:
-        command = f"{COMPILER_COMMAND} '{input_file}' -o {output_file} {COMPILER_ARGUMENT} {COMPILER_ARGUMENT_BIG5}"
-        result = subprocess.run(command, shell=True, capture_output=True)
-        if result == 1:
-            command = f"{COMPILER_COMMAND} '{input_file}' -o {output_file} {COMPILER_ARGUMENT}"
-            result = subprocess.run(command, shell=True, capture_output=True)
-        return result
+    logging.debug(f'start to compile file')
+    command = f"{COMPILER_COMMAND} '{input_file}' -o {output_file} {COMPILER_ARGUMENT} {COMPILER_ARGUMENT_BIG5}"
+    result = subprocess.run(command, shell=True, capture_output=True)
+    if result.returncode == 0:
+        logging.debug(f'compile success')
+        return result.returncode
+    logging.debug(f'compile fail, try to compile without big5')
+    command = f"{COMPILER_COMMAND} '{input_file}' -o {output_file} {COMPILER_ARGUMENT}"
+    result = subprocess.run(command, shell=True, capture_output=True)
+    if result.returncode == 0:
+        logging.debug(f'compile success')
+        return result.returncode
+    logging.debug(f'compile fail second time, return fail result')
+    return result.returncode
+    
 
 
 def command_with_stdin(parameters, target):
@@ -72,35 +96,49 @@ def check_answer(pattern, output_text):
 
 
 def get_directory_name(root):
-    prefix_pattern = f'\.\/{target_week}\/'
+    prefix_pattern = f'\.\/{TARGET_WEEK}\/'
     regex = re.search(prefix_pattern, root)
     return root[regex.span()[1]:]
 
 
 def executor(root, parameters, pattern):
     info = get_directory_name(root)
+    logging.debug(f'start to execute file')
     command = command_with_stdin(parameters, f'{root}/{COMPILE_OUTPUT}')
     try:
         output_text = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as error:
+        logging.debug(f'execute fail, maybe complie fail.')
         return [info, COMPILE_FAIL]
+    logging.debug(f'execute success, start check the answer')
     answer = check_answer(pattern, output_text)
+    logging.debug(f'check the answer complete')
     return [info, answer]
 
 
 if __name__ == '__main__':
+    logging.info(f'start with: {TARGET_WEEK}')
+    logging.debug(f'start for traverse folder')
     buffer = []
     for root, dirs, files in os.walk(TRAVERSE_DIRECTORY):
         if len(files) == 0:
             continue
         for file in files:
-            res = compiler(root, file)
-            if res is None:
+            logging.debug('===================================================')
+            logging.debug(f'current file name: {get_directory_name(root)}/{file}')
+            if check_file_ext(root, file):
+                if DEBUG:
+                    input("Press the Enter key to proceed...")
+                res = compiler(root, file)
+            else:
                 continue
-            elif res == 1:
+            if res == 1:
+                logging.debug(f'compile fail, store the result')
                 ans = [get_directory_name(root), COMPILE_FAIL]
             else:
                 ans = executor(root, FILE_INPUT, OUTPUT_PATTERN)
+            logging.debug(f'execute file success, store the result')
+            logging.debug(f'ans: {ans}')
             buffer.append(ans)
     df = pd.DataFrame(buffer)
     Path(CSV_FOLDER).mkdir(parents=True, exist_ok=True)
